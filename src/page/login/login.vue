5535b87eb6361ad40d227bcb55993891539da295
蒋亚林 <18725944157@163.com>
2018-05-04 17:50:22 +0800

    long皮肤配置

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -1,14 +1,33 @@
 <template>
-  <div class="m-lgn">
-
+  <div class="m-lgn" :style="'background-image: url(' + skin.img + ')'">
+    这是登录
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   export default {
-    name: '',
+    name: 'login',
     data () {
       return {
+        skin: {
+          bgc: {
+            backgroundColor: 'rgba(' + 255 + ',' + 255 + ',' + 255 + ',' + 0.4 + ')',
+            boxShadow: 0 + ' ' + 1 + 'px ' + 2 + 'px' + ' rgba(' + 0 + ',' + 0 + ',' + 0 + ',' + 0.1 + ')'
+          },
+          bgi: {
+            backgroundImage: 'url(' + '"http://192.168.17.111:8088/skiin/bg-02.jpg"' + ')',
+            backgroundSize: 1920 + 'px',
+            filter: 'blur(' + 4 + 'px)'
+          },
+          img: 'http://192.168.17.111:8088/skiin/bg-02.jpg',
+          box: {
+            opacity: 0.95,
+            backgroundColor: 'rgba(' + 4 + ',' + 18 + ',' + 19 + ',' + 0.9 + ')'
+          },
+          font: {
+            color: [255, 255, 255, 0.95]
+          }
+        }
       }
     },
     components: {},
@@ -22,6 +41,12 @@
 </script>
 
 <style scoped>
+  .m-lgn{
+    width: 100%;
+    height: 100%;
+    background-size: 1920px;
+    background-position: center 0;
+  }
 </style>
 
 <style>
