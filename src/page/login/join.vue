c95af90e3355b3efa2b27e02382560f644ac6d3a
蒋亚林 <18725944157@163.com>
2018-05-08 10:05:08 +0800

    登录，注册模块，皮肤合并

--- src/page/login/join.vue
+++ src/page/login/join.vue
@@ -50,28 +50,10 @@
       return {
         eMail: '',
         pwd: '',
-        pwd2: '',
-        skin: {
-          bgc: {
-            backgroundColor: 'rgba(' + 255 + ',' + 255 + ',' + 255 + ',' + 0.4 + ')',
-            boxShadow: 0 + ' ' + 1 + 'px ' + 2 + 'px' + ' rgba(' + 0 + ',' + 0 + ',' + 0 + ',' + 0.1 + ')'
-          },
-          bgi: {
-            backgroundImage: 'url(' + '"http://192.168.17.111:8088/skiin/bg-02.jpg"' + ')',
-            backgroundSize: 1920 + 'px',
-            filter: 'blur(' + 4 + 'px)'
-          },
-          img: 'http://192.168.17.111:8088/skiin/bg-02.jpg',
-          box: {
-            opacity: 0.95,
-            backgroundColor: 'rgba(' + 4 + ',' + 18 + ',' + 19 + ',' + 0.9 + ')'
-          },
-          font: {
-            color: [255, 255, 255, 0.95]
-          }
-        }
+        pwd2: ''
       }
     },
+    props: ['skin'],
     components: {},
     methods: {
       // 去登录
--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -45,28 +45,10 @@
     data () {
       return {
         name: '',
-        pwd: '',
-        skin: {
-          bgc: {
-            backgroundColor: 'rgba(' + 255 + ',' + 255 + ',' + 255 + ',' + 0.4 + ')',
-            boxShadow: 0 + ' ' + 1 + 'px ' + 2 + 'px' + ' rgba(' + 0 + ',' + 0 + ',' + 0 + ',' + 0.1 + ')'
-          },
-          bgi: {
-            backgroundImage: 'url(' + '"http://192.168.17.111:8088/skiin/bg-02.jpg"' + ')',
-            backgroundSize: 1920 + 'px',
-            filter: 'blur(' + 4 + 'px)'
-          },
-          img: 'http://192.168.17.111:8088/skiin/bg-02.jpg',
-          box: {
-            opacity: 0.95,
-            backgroundColor: 'rgba(' + 4 + ',' + 18 + ',' + 19 + ',' + 0.9 + ')'
-          },
-          font: {
-            color: [255, 255, 255, 0.95]
-          }
-        }
+        pwd: ''
       }
     },
+    props: ['skin'],
     components: {},
     methods: {
       // 去注册
